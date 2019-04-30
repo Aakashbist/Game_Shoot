e0b2431b9fb2c87ee30b56c9846165f46d40c828
Aakash Bista <abista74@yahoo.com>
2019-05-01 07:44:40 +1000

    game music and trying background image

--- Game_Shooting/Main.cpp
+++ Game_Shooting/Main.cpp
@@ -1,5 +1,9 @@
 #include<SDL.h>
 #include<iostream>
+#include <SDL_image.h>
+#include <SDL_ttf.h>//text to font
+#include "SoundManager.h"
+#include "Setting.h"
 using namespace std;
 
 
@@ -11,15 +15,85 @@ int main(int argc, char** argv) {
 		return -1;
 	}
 
+	//Need to initialise other sub libraries
+	if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
+		cout << "sdl image failed: " << IMG_GetError() << endl;
+		SDL_Quit();
+		system("pause");
+		return -1;
+	}
 
+	//init sdl mixer
+	if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 4096) == -1) {
+		cout << "Mixer error: " << Mix_GetError() << endl;
+		SDL_Quit();
+		system("pause");
+		return -1;
+	}
+
+	// create window
 	SDL_Window* window = SDL_CreateWindow("My Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-		800, 600, SDL_WINDOW_SHOWN);
+		WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
 
+	if (window == NULL)
+	{
+		cout << "window failed: " << SDL_GetError() << endl;
+		system("pause");
+		SDL_Quit(); //cleans up SDL for us
+		return -1;
+	}
+
+	//create a renderer, which helps draw stuff to the window
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+
+	//load image into ram, as a surface we can access and tweak pixel data if we like
+	SDL_Surface* background = SDL_LoadBMP(GAME_BACKGROUND);
+	//convert surface into texture. Texture will be stored in VRAM for our graphics card to use
+	SDL_Texture* knightTexture = SDL_CreateTextureFromSurface(renderer, background);
+	//dont need surface anymore, lets free its memory
+	SDL_FreeSurface(background);
+
+	//region we want to draw from our texture
+	SDL_Rect sourceRect;
+	sourceRect.x =50 ;
+	sourceRect.y = 60;
+
+	//ask gfx card about width and height of our texture and store it on our sourceRect
+	//SDL_QueryTexture(knightTexture, NULL, NULL, &sourceRect.w, &sourceRect.h);
+	sourceRect.w = WINDOW_WIDTH;
+	sourceRect.h = WINDOW_HEIGHT;
+
+
+	//where to draw our knight
+	SDL_Rect destinationRect;
+	destinationRect.x = 100;
+	destinationRect.y = 100;
+	destinationRect.w = sourceRect.w;
+	destinationRect.h = sourceRect.h;
+
+
+
+
+
+
+	//LOAD UP SOME MUSIC
+	Mix_Music* music = Mix_LoadMUS(GAME_SOUND);
+	if (music == NULL) {
+		cout << "Music failed! : " << Mix_GetError() << endl;
+		SDL_Quit();
+		system("pause");
+		return -1;
+	}
+
+	//Play the music!
+	//params: music to play
+	//		how many times to loop the music (-1 play infinitely)
+	Mix_PlayMusic(music, -1);
 
 
 	bool loop = true;
 	while (loop) {
-
 		//Poll inputs
 		SDL_Event event;
 		//loop through all generated inputs
@@ -37,5 +111,14 @@ int main(int argc, char** argv) {
 		}
 
 	}
+
+	//stop playing music BEFORE you release its memory
+	Mix_PausedMusic();
+	//delete song from memory
+	Mix_FreeMusic(music);
+
+	SDL_DestroyWindow(window);
+
+	SDL_Quit();
 	return 0;
 }
---
+++ Game_Shooting/Setting.h
@@ -0,0 +1,6 @@
+#pragma once
+#define WINDOW_HEIGHT 600
+#define WINDOW_WIDTH 800
+
+#define GAME_SOUND "assets/GameSound.ogg"
+#define GAME_BACKGROUND "assets/background.jpg"
